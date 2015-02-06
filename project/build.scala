ab2ed4ae11df7e72a3ad356e2ad0ecf9ae48e01e
Zoe Gagnon <zoe@tumblr.com>
2015-02-06 17:12:10 -0500

    Defining front end sub project

--- project/build.scala
+++ project/build.scala
@@ -1,4 +1,9 @@
 import sbt._
 
 object MasterBuild extends Build {
+  lazy val root = Project(id = "root",
+                          base = file(".")) aggregate(frontend)
+
+  lazy val frontend = Project(id = "front-end",
+                              base = file("front-end"))
 }
