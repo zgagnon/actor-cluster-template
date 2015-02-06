2f18626c848ca3c71f41f4268b84fe04226647bd
Zoe Gagnon <zoe@tumblr.com>
2015-02-06 17:14:12 -0500

    define back-end sub project

--- project/build.scala
+++ project/build.scala
@@ -2,8 +2,11 @@ import sbt._
 
 object MasterBuild extends Build {
   lazy val root = Project(id = "root",
-                          base = file(".")) aggregate(frontend)
+                          base = file(".")) aggregate(frontend, backend)
 
   lazy val frontend = Project(id = "front-end",
                               base = file("front-end"))
+
+  lazy val backend = Project(id = "back-end",
+                             base = file("back-end"))
 }
