1af0b56984d3ecae6e51883d750cc2be676a6c41
Zoe Gagnon <zoe@tumblr.com>
2015-02-06 17:18:59 -0500

    Adding common sub project for code shared between front and back end.

--- project/build.scala
+++ project/build.scala
@@ -2,11 +2,14 @@ import sbt._
 
 object MasterBuild extends Build {
   lazy val root = Project(id = "root",
-                          base = file(".")) aggregate(frontend, backend)
+                          base = file(".")) aggregate(frontend, backend, common)
 
   lazy val frontend = Project(id = "front-end",
-                              base = file("front-end"))
+                              base = file("front-end")) dependsOn(common % "test->test;compile->compile")
 
   lazy val backend = Project(id = "back-end",
-                             base = file("back-end"))
+                             base = file("back-end")) dependsOn(common % "test->test;compile->compile")
+
+  lazy val common = Project(id = "common",
+                            base = file("common"))
 }
